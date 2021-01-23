b12b2a02fc383598ef541bf6e844b94d5b2db74b
Lillian Paris <lillian.paris529@gmail.com>
2021-01-22 18:54:29 -0700

    deploying

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/reactgoogle-search',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Send every other request to the React app
 // Define any API routes before this runs
