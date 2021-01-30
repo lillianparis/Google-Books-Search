7d3b6bdb0fd6ff4acfe32dcd97a937daab0b5504
Lillian Paris <lillian.paris529@gmail.com>
2021-01-30 09:58:26 -0700

    Code is working now

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 // import logo from "./logo.svg";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
 
