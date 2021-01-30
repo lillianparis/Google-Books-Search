bad7d462b4cfe3798de06130f2aeb64ff4ab82a3
Lillian Paris <lillian.paris529@gmail.com>
2021-01-30 10:24:02 -0700

    Fixing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 // import logo from "./logo.svg";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
 
